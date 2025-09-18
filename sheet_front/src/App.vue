fb0b8b371b5cf769c37bcabef30c4a8c6eaaa51f
Wallace Caetano <wallacesantoscs@gmail.com>
2025-09-18 16:50:35 -0300

    feat(frontend): mostrar resposta da API no HelloWorld.vue

--- sheet_front/src/App.vue
+++ sheet_front/src/App.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+const apiMessage = ref('Carregando...');
+
+onMounted(() => {
+  fetch('http://localhost:8000/')
+    .then(res => res.json())
+    .then(data => {
+      apiMessage.value = data.message;
+    })
+    .catch(() => {
+      apiMessage.value = 'Erro ao conectar com a API';
+    });
+})
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <p>{{ apiMessage }}</p>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
